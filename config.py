0bd615dd5c5c11d28c52b69e0483a54187381d91
ojharadheykishan <ojharadheykishan@gmail.com>
2026-04-19 07:19:44 +0000

    Update bot token

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 
 API_HASH = "f150646c78f09b4f88bef191a22539c0"
 API_ID = 26704085
-BOT_TOKEN = "8564747078:AAF39Ekn22SZxQB7ShELURFel981IFhrmoM"
+BOT_TOKEN = "8564747078:AAHhc3VIy9uuEbqpqgahxYxBXGajFyzIFnE"
 CHANNEL_ID = -1003818713803
 LOG_GROUP = -1005121908460
 # Owner(s) of the bot. Keep as a list for filters.user compatibility.
